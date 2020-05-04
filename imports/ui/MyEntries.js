4c64a293681c5c7d9f2fbfacadb5200dfd666423
nelsontky <nelson@ccb.wtf>
2020-05-04 15:39:25 +0800

    Finished main requirements

--- imports/ui/MyEntries.js
+++ imports/ui/MyEntries.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Entries } from "../api/entries.js";
 
 export default class MyEntries extends React.Component {
+  deleteEntry = id => Entries.remove(id);
+
   render() {
     return (
       <ul>
         {this.props.entries.map((entry) => (
           <li key={entry._id}>
+            <button onClick={() => this.deleteEntry(entry._id)}>
+              &times;
+            </button>
             <a href={"/" + entry._id}>{entry.title}</a>
           </li>
         ))}
